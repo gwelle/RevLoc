3397a293f182f3992db5505f3bb0e444ae9dd9a6
gwelle <guillaume.welle.sio@gmail.com>
2015-11-16 17:31:46 +0100

    Constructeur Garagiste(nom,adresse)

--- src/fr/adavis/revLoc/Garagiste.java
+++ src/fr/adavis/revLoc/Garagiste.java
@@ -11,6 +11,20 @@ public class Garagiste {
 	
 	private String nom ;
 	private String adresse ;
+	
+	
+
+
+	/**
+	 * @param nom
+	 * @param adresse
+	 */
+	public Garagiste(String nom, String adresse) {
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+
+
 
 
 	/**
