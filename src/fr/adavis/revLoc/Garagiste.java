06f81c4584cfd70da25cccc6fd2691e542ac1382
gwelle <guillaume.welle.sio@gmail.com>
2015-11-16 17:28:32 +0100

    Construcuteur Garagiste

---
+++ src/fr/adavis/revLoc/Garagiste.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package fr.adavis.revLoc;
+
+/**
+ * @author eleve
+ *
+ */
+public class Garagiste {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
